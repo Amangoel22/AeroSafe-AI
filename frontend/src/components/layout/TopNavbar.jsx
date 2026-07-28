b54e8848bab9c7ac25ff28a8b72bb6e23de34be0
amangoel <aman.goelmg@gmail.com>
2026-07-28 23:33:11 +0530

    logo

--- frontend/src/components/layout/TopNavbar.jsx
+++ frontend/src/components/layout/TopNavbar.jsx
@@ -13,10 +13,6 @@ const TopNavbar = () => {
           <h1 className="text-3xl font-bold text-white tracking-wide">Airports Authority of India</h1>
           <p className="text-sm text-blue-100 font-semibold mt-1 tracking-wide">Regional Head Quarter, NATS Complex, New Delhi </p>
         </div>
-
-        <div className="flex items-center flex-shrink-0">
-          <img src="/aai_logo.png" alt="RAMS Logo" className="h-20 w-20 rounded-full shadow-md" />
-        </div>
       </div>
     </nav>
   );
